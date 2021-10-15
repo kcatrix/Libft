348faca6a901ea62fc6edced70789bf4d86f5641
catrix <kevyn@catrixs-MacBook-Pro.local>
2021-10-15 16:40:28 +0200

    15/10/2021

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: kevyn <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 13:34:29 by kevyn             #+#    #+#             */
-/*   Updated: 2021/10/15 14:26:57 by kevyn            ###   ########.fr       */
+/*   Updated: 2021/10/15 16:39:58 by kevyn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,11 @@ void	ft_bzero(void *s, size_t n)
 	int	i;
 	char 	*str;
 	str = s;
-
+	
 	i = 0;
-	while(str[i] != n)
+	while (i < n)
 	{
-		str[i] = '\0';
+		str[i] = 0;
 		i++;
 	}
-	s = str;
-}
-
-int	main()
-{
-	char	tab[] = "jesuislachaine";
-	char	tab2[] = "jesuislachaine2";
-
-	printf("original = %s\n", tab);
-	bzero(tab, 3);
-	printf("original after apply : %s\n", tab);
-	printf("copy = %s", tab2);
-	ft_bzero(tab2, 3);
-	printf("copy after apply : %s", tab2);
 }
