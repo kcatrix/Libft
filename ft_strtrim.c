8bcee4961b1d7f4b3e18d29586cb2f8b68205466
Kevyn Catrix <kcatrix@c2r4p6.42nice.fr>
2021-10-26 16:16:46 +0200

    26/10/2021

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,22 +6,26 @@
 /*   By: kcatrix <kcatrix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 14:30:54 by kcatrix           #+#    #+#             */
-/*   Updated: 2021/10/26 16:04:16 by kcatrix          ###   ########.fr       */
+/*   Updated: 2021/10/26 16:15:01 by kcatrix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+/* 
+	#1 PARCOUR
+	#2 RPARCOUR
+	#3 Permet de Malloc 
+*/
 char	*ft_strtrim(const char *s1, const char *set)
 {
 	int		size;
 
 	if (!s1 || !set)
 		return (NULL);
-	while (*s1 && ft_strchr(set, *s1)) // Parcours de chaine
+	while (*s1 && ft_strchr(set, *s1))
 		s1++;
 	size = ft_strlen(s1);
-	while (size && ft_strchr(set, s1[size])) // R Parcours de chaine 
+	while (size && ft_strchr(set, s1[size]))
 		size--;
-	return (ft_substr(s1, 0, size + 1)); // Permet de malloc 
+	return (ft_substr(s1, 0, size + 1));
 }
