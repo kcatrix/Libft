09001a48be7973ace8bea670ea869e5794e7181e
Kevyn Catrix <kcatrix@c2r6p1.42nice.fr>
2021-10-27 15:22:05 +0200

    27/10/2021

---
+++ ft_itoa.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcatrix <kcatrix@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/27 08:42:00 by kcatrix           #+#    #+#             */
+/*   Updated: 2021/10/27 15:21:00 by kcatrix          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	len(long nb)
+{
+	int	len;
+
+	len = 0;
+	if (nb < 0)
+	{
+		nb = nb * -1;
+		len++;
+	}
+	if (nb == 0)
+		return (1);
+	while (nb > 0)
+	{
+		nb = nb / 10;
+		len++;
+	}
+	return (len);
+}
+
+char    *ft_itoa(int nb)
+{
+    char    *str;
+    long    n;
+    int     i;
+    n = nb;
+    i = len(n);
+    str = (char *)malloc(sizeof(char) * (i + 1));
+    str[i] = '\0';
+    i--;
+    if (n == 0)
+    {
+        str[0] = '0';
+        return (str);
+    }
+    if (n < 0)
+    {
+        str[0] = '-';
+        n = n * -1;
+    }
+    while (n > 0)
+    {
+        str[i--] = 48 + (n % 10);
+        n = n / 10;
+    }
+    return (str);
+}
