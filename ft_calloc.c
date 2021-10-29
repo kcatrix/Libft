42aaee6b54bedad45fc4d5bea05136c494da49c0
Kevyn Catrix <kcatrix@c2r7p1.42nice.fr>
2021-10-29 17:53:15 +0200

    29/10/2021 S

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: kcatrix <kcatrix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 16:58:19 by kcatrix           #+#    #+#             */
-/*   Updated: 2021/10/28 14:00:42 by kcatrix          ###   ########.fr       */
+/*   Updated: 2021/10/29 17:33:06 by kcatrix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	int		i;
+	size_t	i;
 	char	*str;
 
 	i = 0;
 	str = malloc(size * count);
 	if (!str)
 		return (NULL);
-	while (str[i] != '\0')
+	while (i < (size * count))
 	{
 		str[i] = 0;
 		i++;
