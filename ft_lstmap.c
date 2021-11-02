3417e520fdc487a7e95d49da1f35f888e8be337c
Kevyn Catrix <kcatrix@c2r6p1.42nice.fr>
2021-11-02 17:59:58 +0100

    02/11/2021 S

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: kcatrix <kcatrix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 16:48:49 by kcatrix           #+#    #+#             */
-/*   Updated: 2021/11/02 17:56:50 by kcatrix          ###   ########.fr       */
+/*   Updated: 2021/11/02 17:59:52 by kcatrix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
 	t_list	*lst2;
 	
 	lst2 = malloc(sizeof(t_list));
-	/*if (lst == NULL || (*f) == NULL)
+	if (lst == NULL || (*f) == NULL)
 	{
 		del(lst2);
 		free(lst2);
 		return (lst);
-	}*/
+	}
 	while (lst)
 	{
 		lst2 = (*f)(lst->content);
